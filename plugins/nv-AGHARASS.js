c826197c5d05b656df04bcbf85cb62c3f7f69bc1
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-12 02:12:30 +0100

    Update nv-AGHARASS.js

--- plugins/nv-AGHARASS.js
+++ plugins/nv-AGHARASS.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/timganin.mp3'
+let vn = './audio/timganin.mp3'
 conn.sendFile(m.chat, vn, 'timganin.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
