c5e42298ea92a2685a5460841843285e5599b6a2
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-12 01:52:45 +0100

    Update nv-atencion a clientes.js

--- plugins/nv-atencion a clientes.js
+++ plugins/nv-atencion a clientes.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Chkon nta.mp3'
-conn.sendFile(m.chat, vn, 'Chkon nta.mp3', null, m, true, {
+let vn = './media/Cheb Larbi _Avec Le Temps Tibgheni.mp3'
+conn.sendFile(m.chat, vn, 'Cheb Larbi _Avec Le Temps Tibgheni.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true
 })
 }
-handler.customPrefix = /Chkon nta|chkon nta|Chkoun nta|chkoun nta|شكون نت|شكون نتا|Madtgit b3da|Mab3da tgit kiyi|madidi ilan|madtgit b3da|mab3da tgit kiyi|madidi ilan|ماتكيت كي|ماتكيت بعدا|ممكن نعرف شكون معايا/
+handler.customPrefix = /Avec le temps tibgheni|Avec Le Temps Tibgheni|Cheb Larbi|Cheb larbi|ماكنبغيكش|مكنبغيكش|makanbghikch|Makanbghikch|madidi ilan|madtgit b3da|mab3da tgit kiyi|madidi ilan|ماتكيت كي|ماتكيت بعدا|ممكن نعرف شكون معايا/
 handler.command = new RegExp
 export default handler
