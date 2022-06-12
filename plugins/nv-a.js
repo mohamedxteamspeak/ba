c0fbaa06072381a95a3185dda721875645a8203d
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-12 02:12:58 +0100

    Update nv-a.js

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -2,7 +2,7 @@ import util from 'util'
 import path from 'path'
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/a.mp3'
+let vn = './audio/a.mp3'
 conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
