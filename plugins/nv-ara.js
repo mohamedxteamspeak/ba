22d2b0ff999bcfec801150de53cd65b6aa7af918
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-12 02:13:05 +0100

    Update nv-ara.js

--- plugins/nv-ara.js
+++ plugins/nv-ara.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/7as biya.mp3'
+let vn = './audio/7as biya.mp3'
 conn.sendFile(m.chat, vn, '7as biya.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
