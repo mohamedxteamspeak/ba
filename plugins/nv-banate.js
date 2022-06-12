8da0624bad2dcd4795fcc274be45550128144ed7
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-12 02:13:22 +0100

    Update nv-banate.js

--- plugins/nv-banate.js
+++ plugins/nv-banate.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/hibhob.mp3'
+let vn = './audio/hibhob.mp3'
 conn.sendFile(m.chat, vn, 'hibhob.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
