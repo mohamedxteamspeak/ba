913366ec1872e30722be824e9c1c53ff8d85e186
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-16 01:04:37 +0100

    Update nv-buenas tardes_mp3.js

--- plugins/nv-buenas tardes_mp3.js
+++ plugins/nv-buenas tardes_mp3.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Fin kayn.mp3'
-conn.sendFile(m.chat, vn, 'Fin kayn.mp3', null, m, true, {
+let vn = './audio/ana lkawkawa.mp3'
+conn.sendFile(m.chat, vn, 'ana lkawkawa.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Fin kayn|Fin kayn db|fin kayn|فين كاين|فين كاين دبا|Mahtilit ghila|Mahtlita|Mahtit/
+handler.customPrefix = /شكون نتي|chkon nti|Chkon nti|فين كاين|xkon nti|Xkon nti|ماتكيت كمي|Matgit kmi/
 handler.command = new RegExp
 export default handler
