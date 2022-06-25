3697919a2cb98f30150baab89039ef6576a73a57
mohamedxteamspeak <bogossmr332@gmail.com>
2022-06-25 15:30:10 +0100

    Update youtube-play3.js

--- plugins/youtube-play3.js
+++ plugins/youtube-play3.js
@@ -1,6 +1,6 @@
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] ★彡اسم الأغنية مفقود ، يرجى إدخال الأمر بالإضافة إلى الاسم / عنوان الأغنية彡★™*\n\n*—◉ 𝙴𝚇𝙰𝙼𝙿𝙻𝙴:*\n*#playdoc Cheb Larbi - Avec Le Temps Tibgheni*`
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] ★彡اسم الأغنية مفقود ، يرجى إدخال الأمر بالإضافة إلى الاسم / عنوان الأغنية彡★™*\n\n*—◉ 𝙴𝚇𝙰𝙼𝙿𝙻𝙴:*\n*${usedPrefix + command} Cheb Larbi - Avec Le Temps Tibgheni*`
 let vid = (await youtubeSearch(text)).video[0]
 if (!vid) throw '*[❗𝐈𝐍𝐅𝐎❗] ★彡عذرًا ، لا يمكنني العثور على الصوت / الفيديو ، حاول استخدام اسم / عنوان آخر彡★™*'
 try {
@@ -15,8 +15,8 @@ conn.sendHydrated(m.chat, `
 ⌚ *𝙳𝚄𝚁𝙰𝚃𝙸𝙾𝙽:* ${durationH}
 👀 *𝚅𝙸𝙴𝚆𝚂:* ${viewH}
 `.trim(), author, thumbnail, `${url}`, '𝚄𝚁𝙻', null, null, [
-['مـْـْْـْلُـِـِِـِِِـِِـِـفُـ,ـ صُـ,ـوُتـٌـٌٌـي', `${usedPrefix}yta.2 ${url} yes`],
-['صُـ,ـيغٍـُـُُـُُُـُُُُـُُُـُُـُة الُـِـِِـِِِـِِـِـفُـ,ـيڊيوُ', `${usedPrefix}ytv.2 ${url} yes`]
+['مـْـْْـْلُـِـِِـِِِـِِـِـفُـ,ـ صُـ,ـوُتـٌـٌٌـي', `${usedPrefix}yta.2 ${url}`],
+['صُـ,ـيغٍـُـُُـُُُـُُُُـُُُـُُـُة الُـِـِِـِِِـِِـِـفُـ,ـيڊيوُ', `${usedPrefix}ytv.2 ${url}`]
 ], m)
 }catch(e){
 m.reply('*[❗𝐈𝐍𝐅𝐎❗] ★彡خطأ ، يرجى المحاولة مرة أخرى彡★™*')
